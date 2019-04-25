2a1ba24697ab095294dce37d1926e786b0466709
Alex Kyriakidis <hootlex@icloud.com>
2019-04-25 13:40:13 +0200

    create the first SFC

---
+++ src/components/BigYellowUsername.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="username">{{username}}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+  .username {
+    font-size: 50pt;
+    color: #DCAE1D;
+  }
+</style>
